dc3cf9439c9d1751aa3f960b305836c7c4809254
joshhoneycutt <joshdhh@gmail.com>
2018-06-10 18:57:21 -0400

    1st Commit

---
+++ LocationTrackingApp/app/src/main/java/com/example/joshd/locationtrackingapp/AuthenticatorActivity.java
@@ -0,0 +1,83 @@
+package com.example.joshd.locationtrackingapp;
+
+/*import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.IdentityProvider;
+import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
+import com.amazonaws.mobile.auth.ui.SignInActivity;
+
+public class AuthenticatorActivity extends AppCompatActivity {
+
+    private static final String TAG = "TEST";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator);
+
+        Log.d(TAG, "Made it this far BEFORE");
+
+        final IdentityManager identityManager = AWSProvider.getInstance().getIdentityManager();
+
+        Log.d(TAG, "Made it this far AFTER");
+
+        // Set up the callbacks to handle the authentication response
+        identityManager.login(this, new DefaultSignInResultHandler() {
+            @Override
+            public void onSuccess(Activity activity, IdentityProvider identityProvider) {
+                Toast.makeText(AuthenticatorActivity.this,
+                        String.format("Logged in as %s", identityManager.getCachedUserID()),
+                        Toast.LENGTH_LONG).show();
+                // Go to the main activity
+                final Intent intent = new Intent(activity, MapsActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                activity.startActivity(intent);
+                activity.finish();
+            }
+
+            @Override
+            public boolean onCancel(Activity activity) {
+                return false;
+            }
+        });
+
+        // Start the authentication UI
+        AuthUIConfiguration config = new AuthUIConfiguration.Builder()
+                .userPools(true)
+                .build();
+        SignInActivity.startSignInActivity(this, config);
+        AuthenticatorActivity.this.finish();
+    }
+}
+*/
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+public class AuthenticatorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticator);
+
+        // Add a call to initialize AWSMobileClient
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+                signin.login(AuthenticatorActivity.this, MapsActivity.class).execute();
+            }
+        }).execute();
+    }
+}
